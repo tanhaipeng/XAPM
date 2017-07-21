a7acc612a4b86351688eb04556cca35b08c1c353
tanhaipeng <tanhaipeng@baidu.com>
2017-07-21 14:29:25 +0800

    init server src

---
+++ server-src/xampsrv.go
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2017 XAPM Author. All Rights Reserved.
+ * XAPM TCP Server Drivered by Golang
+ * 2017/7/21, by Tan Haipeng, create
+ */
+
+package main
+
+import "fmt"
+
+func init() {
+
+}
+
+func main() {
+	fmt.Println("XAPM SERVER")
+}
